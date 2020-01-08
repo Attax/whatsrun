86656871444397d64ce0a575e0652f4ae927a9d5
shigang.zou <shigang.zou@way.io>
2020-01-08 18:45:27 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,55 +1,73 @@
-var app = new Vue({
-    el: "#app",
-    data: {
-        domain: location.hostname,
-        languages: [],
-        stackList: [{
-            name: 'Vue',
-            version: '2.6.10',
-            summary: 'a js framework',
-            id: 1,
-        }, {
-            name: 'jQuery',
-            version: '2.10',
-            summary: 'a js library',
-            id: 2,
-        }]
-    },
-    methods: {
-        init: function() {
-            this.initi18n();
-            //this.getStack();
+(function() {
+    var app = new Vue({
+        el: "#J-Whatsrun-App",
+        data: {
+            url: null,
+            languages: [],
+            stackList: [{
+                name: 'Vue',
+                version: '2.6.10',
+                summary: 'a js framework',
+                id: 1,
+            }, {
+                name: 'jQuery',
+                version: '2.10',
+                summary: 'a js library',
+                id: 2,
+            }]
         },
-        initi18n: function() {
-            var _that = this;
-            chrome.i18n.getAcceptLanguages(function(languageList) {
-                _that.languages = languageList;
-            });
+        computed: {
+            domain: function() {
+                if (this.url) {
+                    return this.url.hostname;
+                }
+            }
         },
-        getStack: function() {
-            axios.get('https://www.baidu.com/sugrec', {
-                    params: {
-                        domain: '',
-                        wd: new Date().getTime(),
-                        t: new Date().getTime()
-                    }
-                }).then(function(res) {
-                    console.log(res);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-                .then(function() {
-                    // always executed
+        methods: {
+            init: function() {
+                this.getDomain();
+                this.initi18n();
+            },
+            initi18n: function() {
+                var _that = this;
+                chrome.i18n.getAcceptLanguages(function(languageList) {
+                    _that.languages = languageList;
                 });
-        },
-    },
-    created: function() {
+            },
+            getStack: function() {
+                axios.get('https://www.baidu.com/sugrec', {
+                        params: {
+                            domain: this.domain,
+                            wd: new Date().getTime(),
+                            t: new Date().getTime()
+                        }
+                    }).then(function(res) {
+                        console.log(res);
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    })
+                    .then(function() {
+                        // always executed
+                    });
+            },
 
-        console.log('created')
-    },
-    mounted: function() {
-        console.log('mounted');
-        this.init();
-    },
-});
+            getDomain: function() {
+                var _that = this;
+                //当前页面
+                chrome.tabs.getSelected(null, function(tab) {
+                    _that.url = new URL(tab.url);
+                });
+            }
+
+        },
+        created: function() {
+
+            console.log('created')
+        },
+        mounted: function() {
+            console.log('mounted');
+            this.init();
+        },
+    });
+})();
