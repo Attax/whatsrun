38fffe2afa15c24142deae26a77872b28b0f3fbc
shigang.zou <shigang.zou@way.io>
2020-01-15 15:33:28 +0800

    添加应用信息

---
+++ static/js/app.config.js
@@ -0,0 +1,811 @@
+var appinfo = {
+
+    'Tumblr': {
+        icon: 'Tumblr.ico',
+        homepage: 'http://tumblr.com',
+        priority: 0.9
+    },
+    'TypePad': {
+        icon: 'TypePad.ico',
+        homepage: 'http://typepad.com',
+        priority: 0.9
+    },
+    'Blogger': {
+        icon: 'Blogger.ico',
+        homepage: 'http://blogger.com',
+        priority: 0.9
+    },
+
+    'Webs': {
+        icon: 'webs.ico',
+        homepage: 'http://www.webs.com/',
+        priority: 0.9
+    },
+    'Weebly': {
+        icon: 'weebly.ico',
+        homepage: 'http://www.weebly.com/',
+        priority: 0.9
+    },
+    'Webnode': {
+        icon: 'webnode.ico',
+        homepage: 'http://www.webnode.com/',
+        priority: 0.9
+    },
+    'Jimdo': {
+        icon: 'jimdo.ico',
+        homepage: 'http://www.jimdo.com',
+        priority: 0.9
+    },
+    'Jigsy': {
+        icon: 'jigsy.ico',
+        homepage: 'http://jigsy.com',
+        priority: 0.9
+    },
+    'Yola': {
+        icon: 'yola.ico',
+        homepage: 'http://www.yola.com/',
+        priority: 0.9
+    },
+
+    // ==== cms or framework ====
+    'vBulletin': {
+        icon: 'vBulletin.ico',
+        homepage: 'http://www.vbulletin.com/',
+        priority: 1
+    },
+    'SMF': {
+        title: 'Simple Machines Forum',
+        icon: 'SMF.ico',
+        homepage: 'http://www.simplemachines.org/',
+        priority: 1
+    },
+    'phpBB': {
+        icon: 'phpBB.ico',
+        homepage: 'http://www.phpbb.com',
+        priority: 1
+    },
+    'IPB': {
+        title: 'Invision Power Board',
+        icon: 'IPB.ico',
+        homepage: 'http://invisionpower.com/',
+        priority: 1
+    },
+    'miniBB': {
+        icon: 'miniBB.ico',
+        homepage: 'http://www.minibb.com/',
+        priority: 1
+    },
+    'MyBB': {
+        icon: 'mybb.ico',
+        homepage: 'http://www.mybb.com/',
+        priority: 1
+    },
+    'XenForo': {
+        icon: 'xenforo.ico',
+        homepage: 'http://xenforo.com',
+        priority: 1
+    },
+
+    'Drupal': {
+        icon: 'Drupal.ico',
+        homepage: 'http://drupal.org',
+        priority: 1
+    },
+    'Ubercart': {
+        icon: 'Ubercart.ico',
+        homepage: 'http://www.ubercart.org/',
+        priority: 0.9 // higher priority compared with Drupal
+    },
+
+    'AlphaCMS': {
+        icon: 'alphacms.ico',
+        homepage: 'http://www.mego.com.vn',
+        priority: 1
+    },
+    'TomatoCMS': {
+        icon: 'tomatocms.ico',
+        homepage: 'http://www.tomatocms.com/',
+        priority: 1
+    },
+
+    'WordPress': {
+        icon: 'WordPress.ico',
+        homepage: 'http://wordpress.org',
+        priority: 1
+    },
+    'WPML': {
+        icon: 'WPML.ico',
+        homepage: 'http://wpml.org/',
+        priority: 1.1
+    },
+
+    'bbPress': {
+        icon: 'bbPress.ico',
+        homepage: 'http://bbpress.org',
+        priority: 1
+    },
+    'Movable Type': {
+        icon: 'MovableType.ico',
+        homepage: 'http://www.movabletype.org/',
+        priority: 1
+    },
+    'Serendipity': {
+        icon: 'Serendipity.png',
+        homepage: 'http://www.s9y.org/',
+        priority: 1
+    },
+    'concrete5': {
+        icon: 'concrete5.gif',
+        homepage: 'http://www.concrete5.org',
+        priority: 1
+    },
+
+    'MediaWiki': {
+        icon: 'MediaWiki.ico',
+        homepage: 'http://www.mediawiki.org/',
+        priority: 1
+    },
+    'DokuWiki': {
+        icon: 'DokuWiki.ico',
+        homepage: 'http://www.dokuwiki.org/',
+        priority: 1
+    },
+
+    'OpenACS': {
+        icon: 'openacs.ico',
+        homepage: 'http://openacs.org',
+        priority: 1
+    },
+
+    'Joomla': {
+        icon: 'Joomla.ico',
+        homepage: 'http://joomla.org',
+        priority: 1
+    },
+    'Magento': {
+        icon: 'Magento.ico',
+        homepage: 'http://www.magentocommerce.com/',
+        priority: 1
+    },
+    'XOOPS': {
+        icon: 'XOOPS.ico',
+        homepage: 'http://xoops.org',
+        priority: 1
+    },
+    'Plone': {
+        icon: 'Plone.ico',
+        homepage: 'http://plone.org/',
+        priority: 1
+    },
+    'CMSMadeSimple': {
+        title: 'CMS Made Simple',
+        icon: 'CMSMadeSimple.ico',
+        homepage: 'http://www.cmsmadesimple.org/',
+        priority: 1
+    },
+    'SilverStripe': {
+        icon: 'SilverStripe.png',
+        homepage: 'http://silverstripe.org',
+        priority: 1
+    },
+    'MODx': {
+        icon: 'MODx.ico',
+        homepage: 'http://modxcms.com/',
+        priority: 1
+    },
+    'Amiro.CMS': {
+        icon: 'Amiro.CMS.ico',
+        homepage: 'http://www.amirocms.com/',
+        priority: 1
+    },
+    'JaliosJCMS': {
+        icon: 'JaliosJCMS.ico',
+        homepage: 'http://www.jalios.com',
+        priority: 1,
+        title: 'Jalios JCMS'
+    },
+    'Koobi': {
+        icon: 'Koobi.ico',
+        homepage: 'http://www.dream4.de/cms/',
+        priority: 1
+    },
+    'Liferay': {
+        icon: 'LifeRay.ico',
+        homepage: 'http://www.liferay.com',
+        priority: 1
+    },
+    'TYPO3': {
+        icon: 'TYPO3.ico',
+        homepage: 'http://typo3.org/',
+        priority: 1
+    },
+    'Contao': {
+        icon: 'contao.ico',
+        homepage: 'http://www.contao.org',
+        priority: 1
+    },
+    'Fatwire': {
+        icon: 'Fatwire.ico',
+        homepage: 'http://www.fatwire.com',
+        priority: 1
+    },
+    'PHP-Fusion': {
+        icon: 'PHP-Fusion.ico',
+        homepage: 'http://php-fusion.co.uk/',
+        priority: 1
+    },
+    'PHP-Nuke': {
+        icon: 'PHP-Nuke.ico',
+        homepage: 'http://phpnuke.org/',
+        priority: 1
+    },
+    'WebGUI': {
+        icon: 'WebGUI.ico',
+        homepage: 'http://www.webgui.org/',
+        priority: 1
+    },
+    'ez Publish': {
+        icon: 'eZ.ico',
+        homepage: 'http://ez.no/',
+        priority: 1
+    },
+    'BIGACE': {
+        icon: 'BIGACE.ico',
+        homepage: 'http://www.bigace.de/',
+        priority: 1
+    },
+    'OpenCMS': {
+        icon: 'opencms.ico',
+        homepage: 'http://www.opencms.org/',
+        priority: 1
+    },
+    '1c-bitrix': {
+        icon: '1c-bitrix.ico',
+        homepage: 'http://www.1c-bitrix.ru/',
+        priority: 1
+    },
+    'MojoMotor': {
+        icon: 'mojomotor.ico',
+        homepage: '	http://mojomotor.com',
+        priority: 1
+    },
+    'GetSimple': {
+        icon: 'getsimple.png',
+        homepage: '	http://get-simple.info/',
+        priority: 1
+    },
+    'Perch': { // not yet supported
+        icon: 'perch.ico',
+        homepage: 'http://grabaperch.com/',
+        priority: 1
+    },
+    'DataLifeEngine': {
+        title: 'DataLife Engine',
+        icon: 'datalife.ico',
+        homepage: 'http://dle-news.ru/',
+        priority: 1
+    },
+
+    'Elgg': {
+        icon: 'Elgg.ico',
+        homepage: 'http://www.elgg.org/',
+        priority: 1
+    },
+
+    'DotNetNuke': {
+        icon: 'DotNetNuke.ico',
+        homepage: 'http://www.dotnetnuke.com/',
+        priority: 1
+    },
+    'Sitefinity': {
+        icon: 'Sitefinity.ico',
+        homepage: 'http://www.sitefinity.com/',
+        priority: 1
+    },
+    'SharePoint': {
+        icon: 'SharePoint.png',
+        homepage: 'http://sharepoint.microsoft.com',
+        priority: 1,
+        title: 'Microsoft SharePoint'
+    },
+
+    'ZenPhoto': {
+        icon: 'ZenPhoto.ico',
+        homepage: 'http://www.zenphoto.org',
+        priority: 1
+    },
+    'Gallery2': {
+        icon: 'Gallery2.ico',
+        homepage: 'http://gallery.menalto.com/',
+        priority: 1
+    },
+
+    'Avactis': {
+        icon: 'avactis.ico',
+        homepage: 'http://www.avactis.com',
+        priority: 1
+    },
+    'PrestaShop': {
+        icon: 'PrestaShop.ico',
+        homepage: 'http://www.prestashop.com/',
+        priority: 1
+    },
+    'Prostores': {
+        icon: 'prostores.ico',
+        homepage: 'http://www.prostores.com',
+        priority: 1
+    },
+    'ZenCart': {
+        icon: 'zencart.ico',
+        homepage: 'http://www.zen-cart.com',
+        priority: 1
+    },
+    'ErainCart': {
+        icon: 'eraincart.ico',
+        homepage: 'http://eraincart.com',
+        priority: 1
+    },
+    'Volusion': {
+        icon: 'volusion.ico',
+        homepage: 'http://www.volusion.com',
+        priority: 1
+    },
+    'osCommerce': {
+        icon: 'osCommerce.ico',
+        homepage: 'http://www.oscommerce.com',
+        priority: 1
+    },
+    'OpenCart': {
+        icon: 'opencart.png',
+        homepage: 'http://www.opencart.com',
+        priority: 1
+    },
+
+    'Moodle': {
+        icon: 'moodle.png',
+        homepage: 'http://moodle.org',
+        priority: 1
+    },
+    'SugarCRM': {
+        icon: 'sugarcrm.ico',
+        homepage: 'http://www.sugarcrm.com',
+        priority: 1
+    },
+    'PivotX': {
+        icon: 'pivotx.ico',
+        homepage: 'http://pivotx.net',
+        priority: 1
+    },
+
+    'Shibboleth': {
+        icon: 'shibboleth.png',
+        homepage: 'http://shibboleth.internet2.edu/',
+        priority: 1
+    },
+    'Alfresco': {
+        icon: 'Alfresco.png',
+        homepage: 'http://www.alfresco.com',
+        priority: 1
+    },
+
+    'ClanSphere': {
+        icon: 'ClanSphere.png',
+        homepage: 'http://csphere.eu',
+        priority: 1
+    },
+
+    // ==== js framework ====
+    'jQuery': {
+        icon: 'jQuery.ico',
+        homepage: 'http://jquery.com',
+        priority: 2
+    },
+    'jQuery UI': {
+        icon: 'jQuery_UI.ico',
+        homepage: 'http://jqueryui.com/',
+        priority: 1.9
+    },
+    'jQuery Mobile': {
+        icon: 'jQuery_Mobile.png',
+        homepage: 'http://jquerymobile.com/',
+        priority: 1.9
+    },
+    'ExtJS': {
+        icon: 'ExtJS.ico',
+        homepage: 'http://www.extjs.com/',
+        priority: 2
+    },
+    'Prototype': {
+        icon: 'Prototype.ico',
+        homepage: 'http://www.prototypejs.org/',
+        priority: 2
+    },
+    'Closure': {
+        icon: 'Closure.png',
+        homepage: 'http://code.google.com/closure/',
+        priority: 2
+    },
+    'MooTools': {
+        icon: 'MooTools.ico',
+        homepage: 'http://mootools.net/',
+        priority: 2
+    },
+    'Dojo': {
+        icon: 'dojo.ico',
+        homepage: 'http://www.dojotoolkit.org/',
+        priority: 2
+    },
+    'script.aculo.us': {
+        icon: 'script.aculo.us.ico',
+        homepage: 'http://script.aculo.us/',
+        priority: 1.9
+    },
+    'YUI': {
+        icon: 'YUI.ico',
+        homepage: 'http://developer.yahoo.com/yui/',
+        priority: 2
+    },
+    'Cappuccino': {
+        icon: 'cappuccino.png',
+        homepage: 'http://cappuccino.org/',
+        priority: 2
+    },
+    'RightJS': {
+        icon: 'rightjs.png',
+        homepage: 'http://RightJS.org/',
+        priority: 2
+    },
+    'Spine': {
+        icon: 'spine.ico',
+        homepage: 'http://spinejs.com',
+        priority: 1.9
+    },
+    'Angular': {
+        icon: 'angular.ico',
+        homepage: 'http://angularjs.org/',
+        priority: 1.5
+    },
+
+    // ==== js tools ====
+    'Raphael': {
+        icon: 'raphael.ico',
+        homepage: 'http://raphaeljs.com/',
+        priority: 2.9
+    },
+    'Typekit': {
+        icon: 'typekit.ico',
+        homepage: 'http://typekit.com/',
+        priority: 2.9
+    },
+    'Cufon': {
+        icon: 'cufon.ico',
+        homepage: 'http://cufon.shoqolate.com/',
+        priority: 2.9
+    },
+    'sIFR': {
+        icon: 'sifr.gif',
+        homepage: 'http://www.mikeindustries.com/blog/sifr/',
+        priority: 2.9
+    },
+    'Modernizr': {
+        icon: 'modernizr.ico',
+        homepage: 'http://www.modernizr.com/',
+        priority: 2.9
+    },
+    'Facebook': {
+        title: 'Facebook Social plugins',
+        icon: 'facebook.ico',
+        homepage: 'http://developers.facebook.com/',
+        priority: 2.9
+    },
+    'Twitter': {
+        title: 'Twitter plugins',
+        icon: 'twitter.ico',
+        homepage: 'http://dev.twitter.com',
+        priority: 2.9
+    },
+    'Buzz': {
+        title: 'Google Buzz Button',
+        icon: 'buzz.png',
+        homepage: 'http://www.google.com/buzz/stuff',
+        priority: 2.9
+    },
+    'Plus1': {
+        title: 'Google Plus 1',
+        icon: 'plus1.png',
+        homepage: 'http://www.google.com/+1/button/',
+        priority: 2.9
+    },
+    'AddThis': {
+        icon: 'addthis.ico',
+        homepage: 'http://www.addthis.com',
+        priority: 2.9
+    },
+    'Backbone.js': {
+        icon: 'Backbone.png',
+        homepage: 'http://documentcloud.github.com/backbone/',
+        priority: 1.9
+    },
+    'Underscore.js': {
+        icon: 'Underscore.png',
+        homepage: 'http://documentcloud.github.com/underscore/',
+        priority: 2.9
+    },
+
+    'Head JS': {
+        icon: 'headjs.gif',
+        homepage: 'http://headjs.com',
+        priority: 2.9
+    },
+    'Google Loader': {
+        icon: 'google.ico',
+        homepage: 'http://code.google.com/apis/loader',
+        priority: 2.9
+    },
+
+    'Woopra': {
+        icon: 'woopra.ico',
+        homepage: 'http://www.woopra.com',
+        priority: 3
+    },
+    'OpenWebAnalytics': {
+        icon: 'owa.ico',
+        homepage: 'http://www.openwebanalytics.com',
+        priority: 3
+    },
+    'Google Analytics': {
+        icon: 'Google_Analytics.ico',
+        homepage: 'http://www.google.com/analytics/',
+        priority: 3
+    },
+    'SiteCatalyst': {
+        icon: 'SiteCatalyst.ico',
+        homepage: 'http://www.omniture.com',
+        priority: 3
+    },
+    'Coremetrics': {
+        icon: 'coremetrics.ico',
+        homepage: 'http://www.coremetrics.com',
+        priority: 3
+    },
+    'Quantcast': {
+        icon: 'Quantcast.ico',
+        homepage: 'http://www.quantcast.com/',
+        priority: 3
+    },
+    'Xiti': {
+        title: 'Xiti Tracker',
+        icon: 'xiti.ico',
+        homepage: 'http://xiti.com/',
+        priority: 3
+    },
+    'Piwik': {
+        icon: 'Piwik.ico',
+        homepage: 'http://piwik.org/',
+        priority: 3
+    },
+    'Clicky': {
+        icon: 'clicky.ico',
+        homepage: 'http://getclicky.com/',
+        priority: 3
+    },
+
+    'Disqus': {
+        icon: 'Disqus.ico',
+        homepage: 'http://disqus.com/',
+        priority: 1
+    },
+    'GetSatisfaction': {
+        icon: 'GetSatisfaction.gif',
+        homepage: 'http://getsatisfaction.com',
+        priority: 3
+    },
+    'Wibiya': {
+        icon: 'Wibiya.ico',
+        homepage: 'http://wibiya.com/',
+        priority: 3
+    },
+    'Prettify': {
+        title: 'Google Code Prettify',
+        icon: 'google.ico',
+        homepage: 'http://code.google.com/p/google-code-prettify/',
+        priority: 3
+    },
+    'reCaptcha': {
+        icon: 'reCaptcha.ico',
+        homepage: 'http://recaptcha.net/',
+        priority: 4
+    },
+    'Mollom': {
+        icon: 'mollom.ico',
+        homepage: 'http://mollom.com',
+        priority: 4
+    },
+    'GoogleFontApi': {
+        title: 'Google Font API',
+        icon: 'google-font-api.gif',
+        homepage: 'http://code.google.com/apis/webfonts/',
+        priority: 4
+    },
+    'GoogleMapApi': {
+        title: 'Google Map API',
+        icon: 'gmap.png',
+        homepage: 'http://code.google.com/apis/maps/',
+        priority: 4
+    },
+    'SWFObject': {
+        icon: 'SWFObject.ico',
+        homepage: 'http://code.google.com/p/swfobject/',
+        priority: 4
+    },
+
+    'OpenX': {
+        icon: 'OpenX.ico',
+        homepage: 'http://openx.org',
+        priority: 5
+    },
+    'AdSense': {
+        icon: 'AdSense.gif',
+        homepage: 'https://www.google.com/adsense',
+        priority: 5
+    },
+    'Chitika': {
+        icon: 'chitika.ico',
+        homepage: 'http://chitika.com/',
+        priority: 5
+    },
+    'BuySellAds': {
+        icon: 'buysellads.ico',
+        homepage: 'http://buysellads.com/',
+        priority: 5
+    },
+    'HumansTxt': {
+        icon: 'humanstxt.ico',
+        homepage: 'http://humanstxt.org/',
+        priority: 5
+    },
+    'Bootstrap': {
+        icon: 'bootstrap.ico',
+        homepage: 'http://twitter.github.com/bootstrap/',
+        priority: 5
+    },
+    'Ning': {
+        icon: 'ning.ico',
+        homepage: 'http://www.ning.com/',
+        priority: 1
+    },
+    'ektron': {
+        icon: 'ektron.ico',
+        homepage: 'http://www.ektron.com/',
+        priority: 1
+    },
+    'Mura CMS': {
+        title: 'Mura CMS',
+        icon: 'muracms.ico',
+        homepage: 'http://www.getmura.com/',
+        priority: 1
+    },
+    'Tiki Wiki CMS Groupware': {
+        title: 'Tiki Wiki CMS Groupware',
+        icon: 'TikiWikiCms.ico',
+        homepage: 'http://info.tiki.org/',
+        priority: 1
+    },
+    'etracker': {
+        title: 'etracker',
+        icon: 'etracker.ico',
+        homepage: 'http://etracker.com/',
+        priority: 1.2
+    },
+    'OpenTag': {
+        icon: 'OpenTag.ico',
+        homepage: 'http://opentag.qubitproducts.com',
+        priority: 1.2
+    },
+    'SPDY': {
+        icon: 'spdy.ico',
+        homepage: 'http://www.chromium.org/spdy',
+        priority: 5
+    },
+    'KISSmetrics': {
+        icon: 'kissmetrics.ico',
+        homepage: 'http://kissmetrics.com/',
+        priority: 1
+    },
+    'LiveStreet': {
+        icon: 'LiveStreetCms.ico',
+        homepage: 'http://livestreetcms.com/',
+        priority: 1
+    },
+    'PHP': {
+        icon: 'php.ico',
+        homepage: 'http://php.net/',
+        priority: 1.5
+    },
+    'Apache': {
+        icon: 'apache.ico',
+        homepage: 'http://httpd.apache.org/',
+        priority: 1.5
+    },
+    'nginx': {
+        icon: 'nginx.ico',
+        homepage: 'http://nginx.org/en/',
+        priority: 1.5
+    },
+    'Varnish': {
+        icon: 'varnish.ico',
+        homepage: 'https://www.varnish-cache.org/',
+        priority: 1.5
+    },
+    'IIS': {
+        icon: 'iis.ico',
+        homepage: 'http://www.iis.net/',
+        priority: 1.5
+    },
+    'ASP.NET': {
+        icon: 'asp.net.ico',
+        homepage: 'http://www.asp.net/',
+        priority: 1.5
+    },
+    'Nette': {
+        icon: 'nette.ico',
+        homepage: 'http://nette.org/',
+        priority: 1.5
+    },
+    'AwsCloudFront': {
+        icon: 'AwsCloudFront.png',
+        homepage: 'http://aws.amazon.com/cloudfront/',
+        priority: 1
+    },
+    'AwsS3': {
+        icon: 'AwsS3.png',
+        homepage: 'http://aws.amazon.com/s3/',
+        priority: 1
+    },
+    'Youtube': {
+        icon: 'Youtube.ico',
+        homepage: 'http://www.youtube.com/',
+        priority: 1
+    },
+    'Dailymotion': {
+        icon: 'Dailymotion.ico',
+        homepage: 'http://www.dailymotion.com/',
+        priority: 1
+    },
+    'Vimeo': {
+        icon: 'Vimeo.ico',
+        homepage: 'https://vimeo.com/',
+        priority: 1
+    },
+    'FlickR': {
+        icon: 'FlickR.ico',
+        homepage: 'http://www.flickr.com/',
+        priority: 1
+    },
+    'Olark': {
+        icon: 'Olark.png',
+        homepage: 'http://www.olark.com/',
+        priority: 1
+    },
+    'SegmentIO': {
+        icon: 'SegmentIO.ico',
+        homepage: 'https://segment.io',
+        priority: 1
+    },
+    'Optimizely': {
+        icon: 'Optimizely.ico',
+        homepage: 'https://www.optimizely.com/',
+        priority: 1
+    },
+    'Typography': {
+        icon: 'Typography.ico',
+        homepage: 'https://www.typography.com/',
+        priority: 1
+    },
+
+    // ==== misc ====
+    '': { // default
+        icon: 'unknown.jpg',
+        homepage: 'http://google.com/search?q=%s'
+    }
+};
